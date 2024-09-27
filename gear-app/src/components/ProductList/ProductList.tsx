4bf8d27302cb2c8687c84a775024db516ccb1c97
ChathuraD1392 <chathuradilshan1392@gmail.com>
2024-09-27 22:17:12 +0530

    made list odf product

--- gear-app/src/components/ProductList/ProductList.tsx
+++ gear-app/src/components/ProductList/ProductList.tsx
@@ -3,7 +3,10 @@ import ProductCard from "../ProductCard/ProductCard";
 const ProductList = () => {
   const products = [
     { id: 1, title: "Product 1", description: "Description", price: 10 },
+    { id: 1, title: "Product 1", description: "Description", price: 10 },
+    { id: 1, title: "Product 1", description: "Description", price: 10 },
   ];
+
   return (
     <>
       {products.map((product, index) => (
